e2eb165f75cd9cdc4f8743b5002dadd14c9690bf
Martin-Sparks <martin.sparks82@gmail.com>
2020-06-04 20:30:11 +0100

    Re-seed database manualy

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -4,7 +4,13 @@ db.dropDatabase();
 db.bugs.insertMany([
     {
         name: "button not working",
-        description:"Button doesn't click"
+        description:"Button doesn't click",
+        dateCreated:"04062020",
+        timeCreated:"2024",
+        ticketStatus:"Fixed",
+        assignedTo:"Martin",
+        label:"Front End",
+        priorityStatus:"Urgent"
     }
 ]);
 
