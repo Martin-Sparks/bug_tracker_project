50510edea1637cb08733730bc86725f3a249e87d
Martin Sparks <Martin.sparks82@gmail.com>
2020-07-21 19:53:10 +0100

    working togle buttons

--- client/src/components/TicketList.vue
+++ client/src/components/TicketList.vue
@@ -4,6 +4,9 @@
     <div id="tickets-wrapper">
       <button v-on:click="toggleUrgentTickets()" >Show only Urgent Tickets</button>
       <button v-on:click="toggleMediumTickets()" >Show only Medium Tickets</button>
+      <button v-on:click="toggleLowTickets()" >Show only Low Tickets</button>
+      <button v-on:click="showTickets()" >Show All Tickets</button>
+
 
       <ul>
         <!-- <li v-for="(ticket, index) in tickets" :key="index" :ticket="ticket" class="ticket-link" v-on:click="selectTicket(ticket)">Ticket name: {{ticket.name}}
@@ -25,7 +28,9 @@ export default {
   props: ["tickets"],
   data() {
     return {
-      urgentStatus: false
+      urgentStatus: false,
+      mediumStatus: false,
+      lowStatus: false
       // ticketsToDisplay: []
     };
   },
@@ -40,19 +45,43 @@ export default {
     },
     toggleUrgentTickets() {
       this.urgentStatus = !this.urgentStatus;
+    },
+    toggleMediumTickets() {
+      this.mediumStatus = !this.mediumStatus;
+    },
+    toggleLowTickets() {
+      this.lowStatus = !this.lowStatus;
+    },
+    showTickets() {
+      this.urgentStatus = false;
+      this.mediumStatus = false;
+      this.lowStatus = false;
     }
   },
 
   computed: {
     ticketsToDisplay() {
-      if (this.urgentStatus == false) return this.tickets;
-      else
+      if (this.urgentStatus == true) {
+        this.mediumStatus = false;
+        this.lowStatus = false;
         return this.tickets.filter(ticket => {
           return ticket.priorityStatus == "Urgent";
-
         });
-
-   
+      } else if (this.mediumStatus == true) {
+        this.urgentStatus = false;
+        this.lowStatus = false;
+        return this.tickets.filter(ticket => {
+          return ticket.priorityStatus == "Medium";
+        });
+      } else if (this.lowStatus == true) {
+        this.mediumStatus = false;
+        this.urgentStatus = false;
+        return this.tickets.filter(ticket => {
+          return ticket.priorityStatus == "Low";
+        });
+      } else {
+        return this.tickets;
+      }
     }
   }
 };
