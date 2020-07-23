a5e52224b8e674e681edc2dd110eafb4789804ac
Martin Sparks <Martin.sparks82@gmail.com>
2020-07-23 15:03:35 +0100

    img added to data = files

--- server/server.js
+++ server/server.js
@@ -9,18 +9,37 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
+// const fs = require('fs');
+
+
+// const upload = multer({
+//   dest: './uploads',
+// });
+
+// app.post('/upload', upload.single('file'), (req, res) => {
+//   res.json({ file: req.file });
+// });
+
 MongoClient.connect("mongodb://localhost:27017")
   .then((client) => {
     const db = client.db("bugtracker");
+
+    // var bucket = new mongodb.GridFSBucket(db);
+
+    // var Binary = require('mongodb').Binary;
+
     const detailsCollection = db.collection("bugs");
     const usersCollection = db.collection("users");
     const projectsCollection = db.collection("projects");
+    const imgCollection = db.collection("img")
     const detailsRouter = createRouter(detailsCollection);
     const usersRouter = createRouter(usersCollection);
     const projectsRouter = createRouter(projectsCollection);
+    const imgRouter = createRouter(imgCollection);
     app.use("/api/bugs", detailsRouter);
     app.use("/api/users", usersRouter);
     app.use("/api/projects", projectsRouter);
+    app.use("/api/img", imgRouter);
   })
   .catch(console.err);
 
