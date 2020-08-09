a98dfe1370730f3af3a2a8fb21e0e3e825ab7a0b
Martin Sparks <Martin.sparks82@gmail.com>
2020-08-09 18:02:50 +0100

    mvp met

--- client/src/components/SingleProject.vue
+++ client/src/components/SingleProject.vue
@@ -2,15 +2,23 @@
   <div>
     <h2>Project name: {{this.project.project_name}}</h2>
     <p>Lead user: {{this.project.leadUser}}</p>
-    
-   <ul>
-       <li v-for="(ticket, index) in ticketsToDisplay" :key="index" :ticket="ticket" class="ticket-link" v-on:click="selectTicket(ticket)">Ticket name: {{ticket.name}}</li>
-   </ul>
 
+    <ul>
+      <li
+        v-for="(ticket, index) in ticketsToDisplay"
+        :key="index"
+        :ticket="ticket"
+        class="ticket-link"
+        v-on:click="selectTicket(ticket)"
+      >Ticket name: {{ticket.name}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+
+import { eventBus } from "@/main";
+
 export default {
   name: "single-project",
   props: ["project", "tickets"],
@@ -20,15 +28,20 @@ export default {
       projectTickets: [],
     };
   },
-  computed: {
 
-    ticketsToDisplay() {
-        return this.tickets.filter((ticket) => {
-          return ticket.project_name == this.project.project_name;
-        });
+  methods: {
+    selectTicket(ticket) {
+      eventBus.$emit("selected-ticket", ticket);
+      eventBus.$emit("selected-page", "single-ticket");
     },
+  },
 
-    
+  computed: {
+    ticketsToDisplay() {
+      return this.tickets.filter((ticket) => {
+        return ticket.project_name == this.project.project_name;
+      });
+    },
   },
 };
 </script>
