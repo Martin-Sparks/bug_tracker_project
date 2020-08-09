fe6cdf9cbdd897ff630cbbbb83dfe40fca62268d
Dave Campbell <dave@digetl.co.uk>
2020-08-09 10:34:29 +0100

    Adding filtered ticket.vue

---
+++ client/src/components/filtered_tickets.vue
@@ -0,0 +1,17 @@
+<template>
+   <h1>Filtered Tickets</h1>
+</template>
+
+<script>
+import { eventBus } from '@/main';
+
+
+export default {
+    name: "FilteredTickets",
+
+}
+</script>
+
+<style>
+
+</style>
