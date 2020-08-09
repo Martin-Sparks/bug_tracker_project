1b4b7de32e403b3ffcab10b4e95e202fc9a9f4eb
Martin Sparks <Martin.sparks82@gmail.com>
2020-08-09 17:58:38 +0100

    filter tickets now working

--- client/src/components/SingleProject.vue
+++ client/src/components/SingleProject.vue
@@ -2,9 +2,11 @@
   <div>
     <h2>Project name: {{this.project.project_name}}</h2>
     <p>Lead user: {{this.project.leadUser}}</p>
-    <ul>
-      <li v-for="(ticket, index) in projectTickets" :key="index" :ticket="ticket"></li>
-    </ul>
+    
+   <ul>
+       <li v-for="(ticket, index) in ticketsToDisplay" :key="index" :ticket="ticket" class="ticket-link" v-on:click="selectTicket(ticket)">Ticket name: {{ticket.name}}</li>
+   </ul>
+
   </div>
 </template>
 
@@ -19,23 +21,14 @@ export default {
     };
   },
   computed: {
-    findProjectTickets(tickets) {
-    
-      this.tickets.forEach(
-        (ticket) => {
-      
-          if (ticket.project_name == this.project.project_name) {
-            console.log(ticket.project_name + "ticket.project_name");
-            console.log(Object.prototype.toString.call(ticket) + "ticket");
-            projectTickets.push(ticket.project_name);
-            console.log(projectTickets + "projectTickets");
-            console.log("last line");
-          }
-          return this.projectTickets;
-        }
-        
-      );
+
+    ticketsToDisplay() {
+        return this.tickets.filter((ticket) => {
+          return ticket.project_name == this.project.project_name;
+        });
     },
+
+    
   },
 };
 </script>
